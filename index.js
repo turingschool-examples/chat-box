fcdfd9228c6171421d0d61df750dd514d3eaf4d5
Steve Kinney <hello@stevekinney.net>
2016-08-17 08:53:34 -0600

    Add client-side

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const http = require('http').Server(app);
 const cors = require('express-cors');
@@ -7,9 +8,16 @@ const bodyParser = require('body-parser');
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Chat Box'
 app.locals.messages = [];
+
+app.get('/', (request, response) => {
+  fs.readFile(`${__dirname}/public/index.html`, (err, file) => {
+    response.send(file);
+  });
+});
 
 app.get('/messages', (request, response) => {
   response.send({ messages: app.locals.messages });
