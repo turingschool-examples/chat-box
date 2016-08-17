a16f07600ee80edd0200034505267606f13a24cb
Steve Kinney <hello@stevekinney.net>
2016-08-17 11:50:55 -0600

    Add client

--- public/script.js
+++ public/script.js
@@ -1,3 +1,39 @@
-$('.send-message').on('click', (e) => {
+const $userInput = $('.message-user');
+const $messageInput = $('.message-message');
+const $messages = $('.messages');
+
+function getMessages() {
+  $.getJSON('/messages').then(putMessagesOnThePage);
+}
+
+function turnMessageIntoElement(message) {
+  return $(`<article><strong>${message.user}</strong>: ${message.message}</article>`);
+}
+
+function putMessagesOnThePage(data) {
+  var messages = data.messages.map(turnMessageIntoElement);
+  $messages.html(messages);
+}
+
+$('.message-submit').on('click', (e) => {
   e.preventDefault();
+  let user = $userInput.val();
+  let message = $messageInput.val();
+
+  let data = {
+    message: {
+      user: user,
+      message: message
+    }
+  }
+
+  $.post('/messages', data).then(function (message) {
+    getMessages();
+  }).catch(function (error) {
+    console.error('NOOOOOOOOOOOOOO!', error);
+  });
 });
+
+getMessages();
+
+setInterval(getMessages, 5000);
